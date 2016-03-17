4946212e6cdb7cd349f9c2d674e48f143e29fd9a
MTRNord <mtrnord1@gmail.com>
2016-03-17 21:14:25 +0100

    Fix placeholder Test

---
+++ tests/start.js
@@ -0,0 +1,10 @@
+var expect  = require("chai").expect;
+describe("Placeholder", function() {
+  describe("More Placeholder", function() {
+    it("keeps place", function() {
+      var placeholderVar   = 1;
+
+      expect(placeholderVar).to.equal(1);
+    });
+  });
+});
